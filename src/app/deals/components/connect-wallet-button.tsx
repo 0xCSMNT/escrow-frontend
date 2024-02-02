53f83de10bd0e0a5d7af29ac0a1143c3109dd58e
CormacDaly <cormacdaly24@gmail.com>
2024-02-02 09:37:51 +0000

    button added but shadcn not rendering correctly. fix layout.tsx

---
+++ src/app/deals/components/connect-wallet-button.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+
+export function ConnectWalletButton() {
+  return <Button>Connect Wallet</Button>
+}
--- src/app/deals/test/page.tsx
+++ src/app/deals/test/page.tsx
@@ -1,10 +1,10 @@
-// src/app/test/page.tsx
+import { ConnectWalletButton } from "../components/connect-wallet-button";
 
 function TestPage() {
     return (
       <div>
         <h1>Test Page</h1>
-        
+        <ConnectWalletButton />
       </div>
     );
   }
