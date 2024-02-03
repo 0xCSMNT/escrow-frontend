97a624ca99449ef75e74b38cf7bd5f720018a46b
CormacDaly <cormacdaly24@gmail.com>
2024-02-03 13:57:18 +0000

    connect kit added, network not supported

--- src/app/deals/components/connect-wallet-button.tsx
+++ src/app/deals/components/connect-wallet-button.tsx
@@ -1,8 +1,13 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button";
+import { Web3Provider } from "@/app/Web3Provider";
+import { ConnectKitButton } from "connectkit";
 
-
-export function ConnectWalletButton() {   
-  return <Button>Connect Wallet</Button>
+export function ConnectWalletButton() {
+  return (
+    <Web3Provider>
+      <ConnectKitButton />
+    </Web3Provider>
+  );
 }
